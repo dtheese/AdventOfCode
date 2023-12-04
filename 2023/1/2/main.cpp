0f414c9b6ac8c0701fe949eae5acaafd04142db0
David Theese <dtheese@rocky-1.home.theese.com>
2023-12-04 04:39:11 -0700

    Day 1 problem 2

--- 2023/1/2/main.cpp
+++ 2023/1/2/main.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <numeric>
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -8,6 +10,63 @@ using namespace std;
 
 int main()
 {
+   const regex r_first{
+                         "^.*?"
+                         "("
+                            "1|one|"
+                            "2|two|"
+                            "3|three|"
+                            "4|four|"
+                            "5|five|"
+                            "6|six|"
+                            "7|seven|"
+                            "8|eight|"
+                            "9|nine|"
+                            "0|zero"
+                         ")"
+                         ".*$"
+                      };
+
+   const regex r_last{
+                        "^.*"
+                        "("
+                           "1|one|"
+                           "2|two|"
+                           "3|three|"
+                           "4|four|"
+                           "5|five|"
+                           "6|six|"
+                           "7|seven|"
+                           "8|eight|"
+                           "9|nine|"
+                           "0|zero"
+                        ")"
+                        ".*$"
+                     };
+
+   map<string, unsigned int> conversions{
+                                           {"1", 1},
+                                           {"2", 2},
+                                           {"3", 3},
+                                           {"4", 4},
+                                           {"5", 5},
+                                           {"6", 6},
+                                           {"7", 7},
+                                           {"8", 8},
+                                           {"9", 9},
+                                           {"0", 0},
+                                           {"one", 1},
+                                           {"two", 2},
+                                           {"three", 3},
+                                           {"four", 4},
+                                           {"five", 5},
+                                           {"six", 6},
+                                           {"seven", 7},
+                                           {"eight", 8},
+                                           {"nine", 9},
+                                           {"zero", 0}
+                                        };
+
    ifstream input_file("input.txt");
 
    if (! input_file)
@@ -22,23 +81,34 @@ int main()
 
    while (getline(input_file, one_line))
    {
-      string::size_type first_digit_index{one_line.find_first_of("0123456789")};
-      string::size_type last_digit_index{one_line.find_last_of("0123456789")};
+      smatch m_first;
+      string s_first;
 
-      if (first_digit_index == string::npos || last_digit_index == string::npos)
+      smatch m_last;
+      string s_last;
+
+      if (regex_match(one_line, m_first, r_first))
+         s_first = m_first.str(1);
+      else
       {
-         cout << "Digit not found!" << endl;
+         cout << "Bad input string: " << one_line << endl;
 
          return 1;
       }
 
-      char first_digit{one_line[first_digit_index]};
-      char last_digit{one_line[last_digit_index]};
+      if (regex_match(one_line, m_last, r_last))
+         s_last = m_last.str(1);
+      else
+      {
+         cout << "Bad input string: " << one_line << endl;
 
-      string one_number_s{first_digit};
-      one_number_s += last_digit;
+         return 1;
+      }
 
-      all_numbers.push_back(stoul(one_number_s));
+      unsigned int one_num{10 * conversions.at(s_first) + conversions.at(s_last)};
+      all_numbers.push_back(one_num);
+
+      cout << one_line << "   " << s_first << "   " << s_last << "   " << one_num << endl;
    }
 
    cout << accumulate(all_numbers.cbegin(), all_numbers.cend(), 0) << endl;
